abab8de8b7e3ab2602c20c28e099fa13cbcfedfe
my0916 <yosihara0916@gmail.com>
2019-11-11 15:04:48 +0100

    Update STRT2-UPPMAX.sh

--- STRT2-UPPMAX.sh
+++ STRT2-UPPMAX.sh
@@ -10,7 +10,7 @@ Usage: ${PROGNAME} [-o String] [-g genome (required)] [-t transcriptome] [-b Pat
 Options:
   -o, --out               Output file name. (default: output)
   -g, --genome            Genome (hg19/hg38/mm9/mm10/canFam3) for annotation and QC. Required!
-  -t, --trans             Transcriptome (ref{RefSeq}/ens{ENSEMBL}/known{UCSC known genes}) for annotation and QC. Default : ref. NOTE: no ENSEMBL for hg38&mm10, no known genes for canFam3.  
+  -t, --transcriptome     Transcriptome (ref{RefSeq}/ens{ENSEMBL}/known{UCSC known genes}) for annotation and QC. Default : ref. NOTE: no ENSEMBL for hg38&mm10, no known genes for canFam3.  
   -b, --basecalls         /PATH/to/The Illumina basecalls directory. Required!
   -i, --index             /PATH/to/The directory and basename of the HISAT2 index for the reference genome. Required! 
   -c, --center            The name of the sequencing center that produced the reads. (default: center)
